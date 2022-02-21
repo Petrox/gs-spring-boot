b06f05cdc1da43b94112c64f79fad4bef0428ac1
Peter Kopias <peter.kopias@rainboat.io>
2022-02-21 22:32:45 +0100

    trials

--- initial/src/main/java/com/example/springboot/HelloController.java
+++ initial/src/main/java/com/example/springboot/HelloController.java
@@ -11,4 +11,9 @@ public class HelloController {
 		return "Greetings from Spring Boot!";
 	}
 
+	@GetMapping("/hello")
+	public String indexHello() {
+		return "Puszi Milka!";
+	}
+
 }
--- test/run.sh
+++ test/run.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 cd $(dirname $0)
 
-cd ../complete
+#cd ../complete
+#
+#./mvnw clean package
+#ret=$?
+#if [ $ret -ne 0 ]; then
+#exit $ret
+#fi
+#rm -rf target
 
-./mvnw clean package
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf target
-
-./gradlew build
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf build
+#./gradlew build
+#ret=$?
+#if [ $ret -ne 0 ]; then
+#exit $ret
+#fi
+#rm -rf build
 
 cd ../initial
 
